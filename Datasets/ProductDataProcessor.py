20097584137f154d1485e040bc2b58e474584a78
morbenami1 <mor.benami1@mail.huji.ac.il>
2024-09-07 18:57:48 +0300

    added new 100 size dataset without duplicated

--- Datasets/ProductDataProcessor.py
+++ Datasets/ProductDataProcessor.py
@@ -2,14 +2,14 @@ import pandas as pd
 import random
 from datetime import datetime, timedelta
 
-EXCEL_PRODUCT_PATH = r'C:\Users\moric\Documents\CS\year4\B\Food-Planner\dataset\products_data.xlsx'  # TODO change
+EXCEL_PRODUCT_PATH = r'C:\Users\moric\Documents\CS\year4\B\Food-Planner\Datasets\products_data.csv'  # TODO change
 
 
 # will set the future date to a month from now if not filled
 class ProductDataProcessor:
     def __init__(self, sample_size, future_date=None, excel_path=EXCEL_PRODUCT_PATH):
         self.excel_path = excel_path
-        self.products_df = pd.read_excel(self.excel_path)
+        self.products_df = pd.read_csv(self.excel_path)
         self.random_data_set = self.create_smaller_dataset(sample_size, future_date)
 
     def create_smaller_dataset(self, sample_size, future_date=None):
@@ -20,13 +20,17 @@ class ProductDataProcessor:
         :param future_date: The latest possible date for the 'Date' column (format: 'YYYY-MM-DD').
         :return: A pandas DataFrame with the smaller dataset.
         """
-        # Convert future date to datetime
+        # Convert future date to datetime (if needed)
 
-        # Select a random subset of the data
-        smaller_df = self.products_df.sample(n=sample_size).reset_index(drop=True)
+        # Remove duplicates based on the 'Product Name' column
+        unique_products_df = self.products_df.drop_duplicates(subset=['Product Name'])
+
+        # Select a random subset of the unique data
+        smaller_df = unique_products_df.sample(n=min(sample_size, len(unique_products_df))).reset_index(drop=True)
 
         # Assign random dates between now and the future_date
         smaller_df['Date'] = smaller_df['Date'].apply(lambda x: self.generate_random_date(future_date))
+
         return smaller_df
 
     def get_products_df(self):
@@ -57,15 +61,15 @@ class ProductDataProcessor:
 
 
 def main():
-    sample_size = 10  # Number of products in the smaller dataset
-    future_date = '2024-12-31'  # Example future date
-    processor = ProductDataProcessor(sample_size, future_date)
+    sample_size = 100  # Number of products in the smaller dataset
+    # future_date = '2024-12-31'  # Example future date
+    processor = ProductDataProcessor(sample_size)
 
     # Create a smaller dataset with random dates
-    smaller_dataset = processor.create_smaller_dataset(sample_size, future_date)
+    smaller_dataset = processor.create_smaller_dataset(sample_size)
 
     # Save the smaller dataset to a new Excel file
-    output_path = 'smaller_dataset.xlsx'  # Define your output path
+    output_path = 'smaller_products_data.xlsx'  # Define your output path
     processor.save_to_excel(output_path)
 
 
